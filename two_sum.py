2ab1c5ca19e522efbdeb927c47c18ac73e5ce5d2
Mithil Hari <mithilhari@yahoo.com>
2026-01-02 12:49:22 -0800

    initial commit; Two Sum

---
+++ two_sum.py
@@ -0,0 +1,20 @@
+def two_sum(target, arr):
+    # POPULATE COUNTER DIC (pos, complement)
+
+    D = dict()
+    k, j = None, None
+    for i, val in enumerate(arr):
+        if (target-val) in D:
+            k = i
+            j = D[target-val][0]
+        else:
+            D[val] = (i, target - val)
+
+    return (j, k)
+    
+
+
+if __name__ == "__main__":
+    x = int(input("enter a target sum "))
+    arr = [5, 6, 11] 
+    print(two_sum(x, arr))
